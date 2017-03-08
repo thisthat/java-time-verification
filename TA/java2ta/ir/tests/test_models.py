134a44b222e67eaa5f5ef2ca180efd984ab1b8fb
Francesco Spegni <francesco.spegni@gmail.com>
2017-03-08 17:26:07 +0100

    tests are failing
    probably related to issue #37

--- TA/java2ta/ir/tests/test_models.py
+++ TA/java2ta/ir/tests/test_models.py
@@ -4,22 +4,22 @@ from java2ta.ir.models import Project
 
 import pkg_resources
 
-def check_is_open(project):
-    num_attempts = 30
+def check_is_open(project, max_seconds=30):
+    num_attempts = max_seconds
 
     while num_attempts > 0 and not project.is_open():
         # repeat after a short time ...
         sleep(1)
         num_attempts = num_attempts - 1
 
-    assert project.is_open(), "Process '%s' (path='%s',status='%s') took more that %s seconds to open, something is probably wrong ..." % (project.name, project.path, project.status, num_attempts)
+    assert project.is_open(), "Process '%s' (path='%s',status='%s') took more that %s seconds to open, something is probably wrong ..." % (project.name, project.path, project.status, max_seconds)
 
 
 def test_open_project():
 
     test_proj_path = pkg_resources.resource_filename(__name__, "helloworld")
 
-    p = Project("fooproject", "file://%s" % test_proj_path, "localhost:9000")
+    p = Project("helloworld", "file://%s" % test_proj_path, "localhost:9000")
 
     assert p.is_status("closed")
     p.open()
@@ -30,7 +30,7 @@ def test_open_project():
 
 def test_close_wrong_project():
 
-    test_proj_path = pkg_resources.resource_filename(__name__, "helloworlds")
+    test_proj_path = pkg_resources.resource_filename(__name__, "helloworld")
 
     p = Project("foofiedoesnotexist", "file:///%s" % test_proj_path, "localhost:9000")
 
@@ -45,7 +45,7 @@ def test_close_open_project():
 
     test_proj_path = pkg_resources.resource_filename(__name__, "helloworld")
 
-    p = Project("fie", "file://%s" % test_proj_path, "localhost:9000")
+    p = Project("helloworld", "file://%s" % test_proj_path, "localhost:9000")
 
     p.open()
 
@@ -60,7 +60,7 @@ def test_get_files():
  
     test_proj_path = pkg_resources.resource_filename(__name__, "helloworld")
 
-    p = Project("foo", "file://%s" % test_proj_path, "localhost:9000")
+    p = Project("helloworld", "file://%s" % test_proj_path, "localhost:9000")
 
     p.open()
 
@@ -77,7 +77,7 @@ def test_get_mains():
  
     test_proj_path = pkg_resources.resource_filename(__name__, "helloworld")
 
-    p = Project("foo", "file://%s" % test_proj_path, "localhost:9000")
+    p = Project("helloworld", "file://%s" % test_proj_path, "localhost:9000")
 
     p.open()
 
@@ -133,8 +133,37 @@ def test_open_project_bigger_distributed():
     assert "MsgList.java" in files, files
     assert "Lock.java" in files, files
 
+def check_file(file):
 
-def test_get_file():
+    assert isinstance(file, dict)
+
+    assert "code" in file
+    assert "methods" in file
+    assert "imports" in file
+    assert "abstract" in file
+    assert "staticInit" in file
+    assert "accessRight" in file
+    assert "start" in file
+    assert "end" in file
+    assert file["end"] >= file["start"]
+    assert "interface" in file  
+    assert "implmentsInterfaces" in file # TODO: fix name implementsInterfaces
+    assert "lineEnd" in file
+    assert "importsAsString" in file
+    assert "nodeType" in file
+    assert "parent" in file
+    assert "realPackageName" in file
+    assert "path" in file
+    assert "line" in file
+    assert "allMethods" in file
+    assert "name" in file
+    assert "constraint" in file
+    assert "packageName" in file
+    assert "extendClass" in file
+    assert "timeCritical" in file
+    assert "attributes" in file
+
+def test_get_files_dist():
 
     test_proj_path = pkg_resources.resource_filename(__name__, "dist-progs")
 
@@ -145,5 +174,34 @@ def test_get_file():
 
     check_is_open(p)
  
-    file = p.get_file("Lock.java")
-    assert isinstance(file, dict)
+    file_lock = p.get_file("Lock.java")
+
+    check_file(file_lock)
+    assert not file_lock["abstract"] 
+    assert file_lock["interface"]
+    assert file_lock["name"] == "Lock"
+    assert file_lock["path"].endswith("Lock.java")
+    assert len(file_lock["allMethods"]) == 2
+
+    assert file_lock["allMethods"][0]["name"] in ["requestCS", "releaseCS"]
+    assert file_lock["allMethods"][1]["name"] in ["requestCS", "releaseCS"]
+
+    assert file_lock["implmentsInterfaces"] == [ "MsgHandler" ] # TODO fix name
+    assert file_lock["extendClass"] == "Object"
+
+    file_leader = p.get_file("RingLeader.java")
+    check_file(file_leader)
+    assert file_leader["implmentsInterfaces"] == [ "Election" ] # TODO fix name
+    assert len(file_leader["attributes"]) == 4
+    assert file_leader["attributes"][0]["name"] in [ "number", "leaderId", "next", "awake", ]
+    assert file_leader["attributes"][1]["name"] in [ "number", "leaderId", "next", "awake", ]
+    assert file_leader["attributes"][2]["name"] in [ "number", "leaderId", "next", "awake", ]
+    assert file_leader["attributes"][3]["name"] in [ "number", "leaderId", "next", "awake", ]
+
+    assert file_leader["extendClass"] == "Process"
+    assert len(file_leader["allMethods"]) == 4
+    assert file_leader["allMethods"][0]["name"] in [ "startElection", "handleMsg", "getLeader", "RingLeader"]
+    assert file_leader["allMethods"][1]["name"] in [ "startElection", "handleMsg", "getLeader", "RingLeader"]
+    assert file_leader["allMethods"][2]["name"] in [ "startElection", "handleMsg", "getLeader", "RingLeader"]
+    assert file_leader["allMethods"][3]["name"] in [ "startElection", "handleMsg", "getLeader", "RingLeader"]
+
