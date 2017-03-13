eeb4a3666a094f8d20590175ae1f0a7d3eea8d52
giliva <giovanni.liva@aau.at>
2017-03-13 17:26:14 +0100

    Update start script to solve issue #40

--- server/start.sh
+++ server/start.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
 
+
+TOKEN=7YZxXyYBKvz55x1yreQ2
+
 MVN=`which mvn`
+
+MVN_FLAGS=-DskipTests=True
+ARTIFACT_URL=https://rtse-isys.aau.at/giovanni.liva/java-xal/builds/artifacts/development/download?job=deploy
+ARTIFACT_NAME=server.zip
+
+# check for new version
+touch .head_development
+rm -fR server
+LOCAL_HEAD_DEV=`cat .head_development`
+REMOTE_HEAD_DEV=`git fetch; git log development | head -1 | cut -d ' ' -f 2`
+if [ "${LOCAL_HEAD_DEV}" != "${REMOTE_HEAD_DEV}" ]; then
+    printf "Server is not update. "
+    printf "Updating to "
+    echo ${REMOTE_HEAD_DEV}
+    # Update reference in file
+    echo ${REMOTE_HEAD_DEV} > .head_development
+    wget --header="PRIVATE-TOKEN: ${TOKEN}" ${ARTIFACT_URL} --output-document=${ARTIFACT_NAME}
+    unzip ${ARTIFACT_NAME}
+    rm -fR target
+    mkdir target
+    cp server/target/server-*.jar target/
+fi
+
 SERVER_JAR=`ls target/server-*.jar 2> /dev/null | sort -r | head -1`
 SERVER_FLAGS=-debug
-MVN_FLAGS=-DskipTests=True
 
 echo "server jar $SERVER_JAR ..."
 
