08908deb4a1a3ffe0765b341ca5102a7511daed3
giliva <giovanni.liva@aau.at>
2017-05-12 13:33:45 +0200

    Add commit hash to server

--- server/src/main/java/server/StartServer.java
+++ server/src/main/java/server/StartServer.java
@@ -1,6 +1,7 @@
 package server;
 
 import intermediateModelHelper.indexing.mongoConnector.MongoConnector;
+import server.helper.PropertiesFileReader;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -32,6 +33,8 @@ public class StartServer {
 					break;
 			}
 		}
+
+		System.out.println("Version : " + PropertiesFileReader.getGitSha1());
 
 		Callable<Boolean> task = () -> {
 			try {
