a4ec168add21f6b697b8c11cacfb9cd1c84278a0
giliva <giovanni.liva@aau.at>
2017-07-06 13:00:50 +0200

    Add connection to z3

--- 3rdPartyLibraries/installLocalMaven.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mvn install:install-file -Dfile=${DIR}/org.eclipse.jdt.core-3.12.0.v20160516-2131.jar -DpomFile=${DIR}/org.eclipse.jdt.core-3.12.0.v20160516-2131.pom
