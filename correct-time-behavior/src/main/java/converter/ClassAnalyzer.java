f40f94da37585919f9b5f18202f49ae6fee484e4
giliva <giovanni.liva@aau.at>
2017-11-23 13:39:12 +0100

    Change

--- correct-time-behavior/src/main/java/converter/ClassAnalyzer.java
+++ correct-time-behavior/src/main/java/converter/ClassAnalyzer.java
@@ -4,8 +4,9 @@ import intermediateModel.interfaces.IASTMethod;
 import intermediateModel.structure.ASTClass;
 import slicing.Slice;
 import slicing.model.Method;
+import slicing.SLOCCounter;
 import smt.PathGenerator;
-import smt.TranslateReducedModel;
+//import smt.TranslateReducedModel;
 import smt.exception.VariableNotCorrect;
 
 import java.util.ArrayList;
@@ -17,13 +18,33 @@ import java.util.List;
  */
 public class ClassAnalyzer {
 
-    private final TranslateReducedModel translateReducedModel;
+    //private final TranslateReducedModel translateReducedModel;
     HashMap<IASTMethod, Method> reducedModel = new HashMap<>();
     private boolean getModel = false;
 
     public ClassAnalyzer(ASTClass c) {
         reducedModel = Slice.slice(c);
-        translateReducedModel = new TranslateReducedModel();
+        //translateReducedModel = new TranslateReducedModel();
+    }
+
+    public long getSLOC(){
+        long sum = 0;
+        PathGenerator pg = new PathGenerator();
+        for(IASTMethod m : reducedModel.keySet()){
+            Method mm = reducedModel.get(m);
+            List<Method> analyze = pg.generate(mm);
+            boolean cont = false;
+            for(Method a : analyze) {
+                if(cont) continue;
+                sum += SLOCCounter.count(a);
+                try {
+                    cont = getErrors(a).size() > 0;
+                } catch (Exception e) {
+                    cont = true;
+                }
+            }
+        }
+        return sum;
     }
 
     public List<VariableNotCorrect> getErrors(IASTMethod m){
@@ -41,11 +62,11 @@ public class ClassAnalyzer {
             Statistic.incrementNMethod(1);
             Statistic.incrementNMethodPath(analyze.size());
         }
-        translateReducedModel.saveModel(getModel);
+        //translateReducedModel.saveModel(getModel);
         List<VariableNotCorrect> tmp;
         for(Method mm : analyze) {
-            tmp = translateReducedModel.check(mm);
-            out.addAll(tmp);
+            //tmp = translateReducedModel.check(mm);
+            //out.addAll(tmp);
         }
         return out;
     }
