559e9a413732fe95cb7449970799dbec7e993616
giliva <giovanni.liva@aau.at>
2018-02-05 11:26:39 +0100

    test build

--- start_mongo.sh
+++ start_mongo.sh
@@ -3,6 +3,6 @@ mongo --eval "db.stats()"  # do a simple harmless command of some sort
 RESULT=$?   # returns 0 if mongo eval succeeds
 
 if [ $RESULT -ne 0 ]; then
-    mongod &
+    screen -d -m bash mongod
     exit 1
 fi
