ac30926b5068b1d65b315c84f594fd31ff70a3c1
Bondrewd <gabrieleflorio06@gmail.com>
2018-03-08 00:54:12 +0100

    see #1

--- TA/java2ta/ta/views.py
+++ TA/java2ta/ta/views.py
@@ -119,46 +119,98 @@ class GraphViz(TARenderer):
             loc_name = re.sub(r'\@[0-9\.]+$', "", loc_name)
 
         return loc_name
-    def group_locations(loc):
-     con=""
-     cont=0
-     listaii=[]
-     listai=[]
+        
+    def group_locations(self,loc):
+     con=[]
+    # cont=0
      lista=[]
-     while len(loc)!=0:
-      for ta_location in loc:
-       if str(ta_location)=="initial":
-        loc.remove["initial"]
-        continue
-       if str(ta_location)=="ending":
-        loc.remove["ending"]
-        continue
-       if cont==0:
-        cont+=1
-        con=str(ta_location)[1]
-       if str(ta_location)[1]==con:
-        listaii.append(loc.pop(loc.index(ta_location)))
-      listaii.sort()
-      listai.append(con)
-      listai.append(listaii)
-      lista.append(listai)  
-      del listaii[:]
-      del listai[:]
+     #while len(loc)!=0:
+     for ta_lo in loc:
+            if ta_lo.is_initial:
+                continue
+            if con.count(ta_lo.name.split("@")[1])==0:    
+                con.append(ta_lo.name.split("@")[1])
+            print "ecco con prima %s" % (con)
+     while 1:
+        listaii=[]
+        #con=str(cont)
+        
+        cona=con.pop()    
+        for ta_location in loc:
+            print "ecco con dopo %s" % (con)
+           # if str(ta_location)=="initial":
+       # loc.remove("initial")
+              #  continue
+           # if str(ta_location)=="ending":
+       # loc.remove("ending")
+              #  continue
+            #if cont==0:
+             #   cont+=1
+              #  con=str(ta_location)[1]
+            if ta_location.is_initial:
+                continue
+            if ta_location.name.split("@")[1]==cona:
+                
+       # listai.append(loca)
+                listaii.append(ta_location)
+                #loc.remove(ta_location)
+        #cont+=1
+         
+        #listaii.sort()
+        listai=(cona,sorted(listaii)) 
+        lista.append(listai) 
+        print "ecco la lista %s" % (lista)
+        if len(con)==0:
+            break  
+         
+        #del listaii[:]
      return lista
+     
     def render(self, *args, **kwargs):
         ta = self.ta
     
         g = Digraph(ta.name)
         g.attr(rankdir="TB")
-
+        print "legend",str(self.legend)
         
+      #  for (pc, locations) in sorted(self.group_locations(ta.locations)):
+      #     sg = g.subgraph(name=pc)
+      #     sg.attr(rankdir='LR')
+      #     for loc in locations:
+      #         sg.node(loc.name, label=loc_name, **node_attrs)
+        
+        
+        node_attrs = { "shape": "rect", "style": "rounded" }
         for loc in ta.locations:
-            node_attrs = { "shape": "rect", "style": "rounded" }
-            if loc.is_initial:
-                node_attrs["peripheries"] = "2"
-
-            loc_name = self.get_loc_name(loc)
-            g.node(loc.name, label=loc_name, **node_attrs)
+                if loc.is_initial:
+                  #  print str(loc)
+                    loc_name = self.get_loc_name(loc) 
+                    node_attrs["peripheries"] = "2"
+                    g.node(loc.name, label=loc_name, **node_attrs)
+        node_attrs["peripheries"] = "1"                
+        for cluster_id, (pc, locations) in enumerate(sorted(self.group_locations(ta.locations))):
+            print "\necco la lista restituita %s,%s" % (pc,locations)
+          #  c=Digraph(pc)       
+          #  c.attr(rankdir="TB")
+            
+           # node_attrs = { "shape": "oval", "style": "rounded" }
+            
+           
+           # sg = g.subgraph(name=pc)
+           # sg.attr(rankdir='LR')
+            
+           # for loc in locations:
+            with g.subgraph(name=str(cluster_id)) as c:
+                c.attr(rankdir="TB")
+                c.attr(style="invis")
+                for loc in locations:
+                        
+                        loc_name = self.get_loc_name(loc) 
+                        print "ecco l'elemento %s e il nome %s" % (loc,loc_name)
+                        #sg.node(loc.name, label=loc_name, **node_attrs)        
+                        c.node(loc.name, label=loc_name, **node_attrs)
+              #  g.node(loc.name, label=loc_name, **node_attrs)
+          #  g.subgraph(c)  
 
         for e in ta.edges:
             g.edge(e.source.name, e.target.name, label=e.label)
